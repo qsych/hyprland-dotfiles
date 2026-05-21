898fcdbf289065ad68ce8d5d3331dd51d319e13f
qq <qsych>
2026-05-21 14:04:43 +0300

    1

---
+++ hypr/custom/monitors.lua
@@ -0,0 +1,19 @@
+return {
+  monitor = {
+    "desc:Philips Consumer Electronics Company PHL 272B8Q UK01933010545, 2560x1440@60, 0x0, 1",
+    "desc:InfoVision Optoelectronics (Kunshan) Co.Ltd China 0x8596 0x00000004, 1920x1080@60, 320x1440, 1",
+  },
+
+  workspace = {
+    "1, monitor:desc:InfoVision Optoelectronics (Kunshan) Co.Ltd China 0x8596 0x00000004, default:true, persistent:true",
+    "2, monitor:desc:InfoVision Optoelectronics (Kunshan) Co.Ltd China 0x8596 0x00000004",
+    "3, monitor:desc:InfoVision Optoelectronics (Kunshan) Co.Ltd China 0x8596 0x00000004",
+    "4, monitor:desc:InfoVision Optoelectronics (Kunshan) Co.Ltd China 0x8596 0x00000004",
+    "5, monitor:desc:InfoVision Optoelectronics (Kunshan) Co.Ltd China 0x8596 0x00000004",
+    "6, monitor:desc:Philips Consumer Electronics Company PHL 272B8Q UK01933010545, default:true, persistent:true",
+    "7, monitor:desc:Philips Consumer Electronics Company PHL 272B8Q UK01933010545",
+    "8, monitor:desc:Philips Consumer Electronics Company PHL 272B8Q UK01933010545",
+    "9, monitor:desc:Philips Consumer Electronics Company PHL 272B8Q UK01933010545",
+    "10, monitor:desc:Philips Consumer Electronics Company PHL 272B8Q UK01933010545",
+  },
+}
